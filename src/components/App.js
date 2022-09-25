d402b33000bdcc53518a0cc414c6a7f0b2bce5c5
SONU-GUPTA1 <109684425+SONU-GUPTA1@users.noreply.github.com>
2022-09-25 15:45:13 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,16 @@
-// import React, {Component, useState} from "react";
-// import "./../styles/App.css";
-// let name = " Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy";
-
-// function App() {
-//    const[data,setData] = useState(" ")
-//   const change = ()=>{
-//     setData(name)
-//   }
-//   return (
-//     <div id="main">
-//      <button id="click" onClick={change} >click</button>
-//       <p id="para" >{data}</p>
-      
-//     </div>
-//   );
-// }
-
-
-// export default App;
-
 import React, {Component, useState} from "react";
 import "./../styles/App.css";
-let name = "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy";
+let name = " Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy";
 
 function App() {
+   const[data,setData] = useState(" ")
+  const change = ()=>{
+    setData(name)
+  }
   return (
     <div id="main">
-      <Component value={name}/>
+     <button id="click" onClick={change} >click</button>
+      <p id="para" >{data}</p>
       
     </div>
   );
@@ -34,3 +18,4 @@ function App() {
 
 
 export default App;
+
