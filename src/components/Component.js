b4775c9e5bea14a7f79cbe3b61b6b4daabf05520
SONU-GUPTA1 <109684425+SONU-GUPTA1@users.noreply.github.com>
2022-09-25 15:36:59 +0530

    Update Component.js

--- src/components/Component.js
+++ src/components/Component.js
@@ -1,1 +1,18 @@
 
+import {useState} from "react"
+
+function Component(props) {
+  const[data,setData] = useState(" ")
+  const change = ()=>{
+    setData(props.value)
+  }
+  return (
+    <div>
+      <button id="click" onClick={change}>
+        click
+      </button>
+      <p id="para">{data}</p>
+    </div>
+  );
+}
+export default Component;
